c4ac87ff39c01bb74cc540511f428d9ad149b634
midasworld <midas117@knou.ac.kr>
2021-11-28 21:34:54 +0900

    feat : 프로젝트 생성

---
+++ src/test/java/com/midas/marketservice/MarketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midas.marketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
